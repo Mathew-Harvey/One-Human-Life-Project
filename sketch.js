370c24e43d582e1f216d3312f9f24d3450384e19
mat.harvey@rise-x.io <mat.harvey@rise-x.io>
2022-07-02 22:22:11 +0800

    form submission complete

--- sketch.js
+++ sketch.js
@@ -4,13 +4,6 @@ var cells = [];
 var userAge = 45
 
 
-function myFunction() {
-  const test = document.getElementById("search").value;
-  console.log(test);
-  
-}
-
-
 function make2Darray(cols, rows) {
   var arr = new Array(cols);
   for (var i = 0; i < arr.length; i++) {
@@ -20,14 +13,17 @@ function make2Darray(cols, rows) {
 }
 
 function setup() {
+  var ageInWeeks = document.getElementById("search").value;
+  var ageInYears = ageInWeeks * 52
   var canvas = createCanvas(1040, 2000);
   canvas.parent('timeLeft');
   cells = make2Darray(cols, rows);
   for (var i = 0; i < cols; i++) {
     cells[j] = [];
     for (var j = 0; j < rows; j++) {
+
       let index = j * cols + i;
-      if (index > ageInWeeks) {
+      if (index > ageInYears) {
         fill(255);
       } else {
         fill(100);
@@ -46,6 +42,3 @@ function setup() {
 function draw() {
   textAlign(CENTER, CENTER);
 }
-
-var ageInYears = userAge;
-var ageInWeeks = ageInYears * 52;
